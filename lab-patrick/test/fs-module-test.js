ba3fbdd16b12b36dae3ed3021b6cbad6a782ed92
Patrick Sheridan <sheridp2@gmail.com>
2017-04-19 18:12:20 -0700

    one test written

---
+++ lab-patrick/test/fs-module-test.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const expect = require('chai').expect;
+const fs = require('fs');
--- lab-patrick/test/test.js
+++ lab-patrick/test/test.js
@@ -2,3 +2,27 @@
 
 const expect = require('chai').expect;
 const fs = require('fs');
+
+describe('fs-module', function(){
+  describe('#readFile', function(){
+    it('should read a file and produce the first 8 bytes in hex', function(){
+      fs.readFile(`../data/one.txt`, function(err, data){
+        if(err) throw err;
+        let oneData = data.toString('hex', 0, 8);
+        console.log('one', oneData);
+
+        fs.readFile(`../data/two.txt`, function(err, data){
+          if(err) throw err;
+          let twoData = data.toString('hex', 0, 8);
+          console.log('two', twoData);
+
+          fs.readFile(`../data/three.txt`, function(err, data){
+            if(err) throw err;
+            let threeData = data.toString('hex', 0, 8);
+            console.log('three', threeData);
+          });
+        });
+      });
+    });
+  });
+});
