859ceed9e86ef65051c0ab160a6aa6ee762323e2
Patrick Sheridan <sheridp2@gmail.com>
2017-04-19 18:11:35 -0700

    one test written

--- lab-patrick/lib/fs-module.js
+++ lab-patrick/lib/fs-module.js
@@ -4,21 +4,18 @@ const fs = require('fs');
 
 fs.readFile(`../data/one.txt`, function(err, data){
   if(err) throw err;
-  // console.log(data);
   let oneData = data.toString('hex', 0, 8);
-  console.log(oneData);
-});
+  console.log('one', oneData);
 
-fs.readFile(`../data/two.txt`, function(err, data){
-  if(err) throw err;
-  // console.log(data);
-  let oneData = data.toString('hex', 0, 8);
-  console.log(oneData);
-});
+  fs.readFile(`../data/two.txt`, function(err, data){
+    if(err) throw err;
+    let twoData = data.toString('hex', 0, 8);
+    console.log('two', twoData);
 
-fs.readFile(`../data/three.txt`, function(err, data){
-  if(err) throw err;
-  // console.log(data);
-  let oneData = data.toString('hex', 0, 8);
-  console.log(oneData);
+    fs.readFile(`../data/three.txt`, function(err, data){
+      if(err) throw err;
+      let threeData = data.toString('hex', 0, 8);
+      console.log('three', threeData);
+    });
+  });
 });
