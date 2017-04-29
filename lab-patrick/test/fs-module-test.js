831da59b24be10fe44a13f4854f1bd95eabb9f12
Patrick Sheridan <sheridp2@gmail.com>
2017-04-29 14:17:58 -0700

    test added and passing

--- lab-patrick/test/fs-module-test.js
+++ lab-patrick/test/fs-module-test.js
@@ -2,23 +2,33 @@
 
 const expect = require('chai').expect;
 const fs = require('fs');
-
+const textArray = ['./data/one.txt', './data/two.txt', './data/three.txt'];
+const hexArray = [];
+const testData = ['6f6d6d6f646f2063','4865726520617265','4c6f72656d206970'];
 
 describe('fs-module', function(){
   describe('#readFile', function(){
-    it('should check that the text files display in order', function(){
+    it('should produce an array', done=>{
+      fs.readFile(textArray[0], (err, data) => {
+        if(err) throw err;
+        hexArray.push(data.toString('hex', 0, 8));
 
-      let hexArray = [];
-      const textArray = ['../data/one.txt', '../data/two.txt', '../data/three.txt'];
-      for (var i = 0; i < textArray.length; i++ ) {
-        fs.readFile(textArray[i], function(err, data){
+        fs.readFile(textArray[1], (err, data) => {
           if(err) throw err;
-          let dataI = data.toString('hex', 0, 8);
-          console.log(dataI);
-          hexArray.push(dataI);
+          hexArray.push(data.toString('hex', 0, 8));
+
+          fs.readFile(textArray[2], (err, data) => {
+            if(err) throw err;
+            hexArray.push(data.toString('hex', 0, 8));
+
+            expect(hexArray[0]).to.equal(testData[0]);
+            expect(hexArray[1]).to.equal(testData[1]);
+            expect(hexArray[2]).to.equal(testData[2]);
+
+            done();
+          });
         });
-        expect(hexArray[0]).to.equal('6f6d6d6f646f2063');
-      }
+      });
     });
   });
 });
